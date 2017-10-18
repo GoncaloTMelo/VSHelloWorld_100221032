c41ec42e834304c2798262c5ba5ed2bf21176e66
GTMelo <GTMelo@gmail.com>
2017-10-18 12:35:44 +0100

    Adição de um Comentário

--- VSHelloWorld_100221032/Program.cs
+++ VSHelloWorld_100221032/Program.cs
@@ -11,6 +11,7 @@ namespace VSHelloWorld_100221032
     {
         public static void Main(string[] args)
         {
+            // Comentário de Teste
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
